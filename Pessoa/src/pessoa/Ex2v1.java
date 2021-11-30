daa6065105d1bec6ee23618ca8b0a0d8b521f350
Nikolas Caldeira <62756507+NikolasCaldeira@users.noreply.github.com>
2021-11-29 21:52:43 -0300

    Update Ex2v1.java

--- Pessoa/src/pessoa/Ex2v1.java
+++ Pessoa/src/pessoa/Ex2v1.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import pessoa.Pessoa;
 
 public class Ex2v1 implements ActionListener {
 
